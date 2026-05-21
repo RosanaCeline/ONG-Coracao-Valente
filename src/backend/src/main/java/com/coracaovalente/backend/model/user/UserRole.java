5dd3f685e1d77f0141ed7c07e6c62d5c4dd7e230
Rosana Celine <celine.rosana@gmail.com>
2026-05-21 16:11:27 -0300

    fix: add token response and user roles

--- src/backend/src/main/java/com/coracaovalente/backend/model/user/UserRole.java
+++ src/backend/src/main/java/com/coracaovalente/backend/model/user/UserRole.java
@@ -2,5 +2,15 @@ package com.coracaovalente.backend.model.user;
 
 public enum UserRole {
     ADMIN("admin"),
-    USER("user")
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String userRole() {
+        return role;
+    }
 }
