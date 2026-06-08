7e1b4b6548d47896135069f100731027b9600ed6
Laís Coutinho <laiscc575@gmail.com>
2026-06-07 21:53:07 -0300

    fix: update AppRoutes to default imports after page export refactor

--- src/frontend/src/routes/AppRoutes.jsx
+++ src/frontend/src/routes/AppRoutes.jsx
@@ -2,16 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Contact from "../pages/Contact/Contact";
-import { Adoption } from "../pages/Adoption/Adoption";
-import { Help } from "../pages/Help/Help";
-import { Donate } from "../pages/Donate/Donate";
+import Adoption from "../pages/Adoption/Adoption";
+import Help from "../pages/Help/Help";
+import Donate from "../pages/Donate/Donate";
 import { Login } from "../pages/Login/Login";
+import History from "../pages/History/History";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/historia" element={<Contact />} />
+      <Route path="/historia" element={<History />} />
       <Route path="/adocao" element={<Adoption />} />
       <Route path="/voluntariado" element={<Help />} />
       <Route path="/contato" element={<Contact />} />
